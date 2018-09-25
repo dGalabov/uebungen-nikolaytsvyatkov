64b338242429bf220817a7a40a96e280b7ed6d71
201218055 <201218055@STUDWS208-05.FDIBA.local>
2018-09-25 19:06:41 +0300

    alabala

---
+++ uebungen/src/anfang/PrintExample.java
@@ -0,0 +1,13 @@
+package anfang;
+
+public class PrintExample {
+	public static void main(String[] args) {
+		System.out.println("My name is Alan Turing.");
+		System.out.println("I was born in 1912.");
+		System.out.println("I am the father of");
+		System.out.println("trheoretical computer science");
+		System.out.println("and artificcal intellegence");
+		
+	}
+
+}
---
+++ uebungen/src/anfang/Weihnachtsbaum.java
@@ -0,0 +1,16 @@
+package anfang;
+
+public class Weihnachtsbaum {
+	public static void main(String[] args) {
+		System.out.println("    * ");
+		System.out.println("   *** ");
+		System.out.println("  *****");
+		System.out.println(" ******");
+		System.out.println("*********");
+		System.out.println("  ***");
+		
+		
+	}
+	
+
+}
